fc42ec4331f3f808d7b1156c2482280a56f9a9b0
rainap-ui <prashansa312@gmail.com>
2022-07-11 15:49:05 +0530

    first

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.12.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  client_certificate = base64decode(var.kubeclientcert)
+  client_key = base64decode(var.kubeclientkey)
+  cluster_ca_certificate = base64decode(var.kubeclustercert)
+  }
+
+resource "kubernetes_deployment" "python-docker-deployment" {
+
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind"       = "Deployment"
+    "metadata" = {
+      "labels" = {
+        "app" = "sample-app"
+      }
+      "name"      = "sample-app"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "replicas" = 2
+      "selector" = {
+        "matchLabels" = {
+          "app" = "sample-app"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = "sample-app"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "image" = "rainap/python-docker"
+              "name"  = "sample-app"
+              "ports" = [
+                {
+                  "containerPort" = 80
+                  "protocol"      = "TCP"
+                },
+              ]
+            },
+          ]
+        }
+      }
+    }
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "kubeclientcert" {
+  type = string
+}
+
+variable "kubeclientkey" {
+  type = string
+}
+
+variable "kubeclustercert" {
+  type = string
+}
